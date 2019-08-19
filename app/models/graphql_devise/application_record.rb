cfe7cd80a25dbf0844cc736808654c21ac30ccb0
Mario Celi <mcelicalderon@gmail.com>
2019-08-19 10:44:15 -0500

    Remove unnecessary spec and model

--- app/models/graphql_devise/application_record.rb
+++
@@ -1,5 +0,0 @@
-module GraphqlDevise
-  class ApplicationRecord < ActiveRecord::Base
-    self.abstract_class = true
-  end
-end
--- spec/graphql_devise_spec.rb
+++ spec/graphql_devise_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe GraphqlDevise do
   it 'has a version number' do
     expect(GraphqlDevise::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(true).to eq(true)
-  end
 end
