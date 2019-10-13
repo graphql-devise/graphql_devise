75a48dd0e9c9776a972c5d49ae613e7e0feaf419
Mario Celi <mcelicalderon@gmail.com>
2019-10-13 16:13:59 -0500

    Support skipping operations when mounting routes

--- lib/graphql_devise/rails/routes.rb
+++ lib/graphql_devise/rails/routes.rb
@@ -1,7 +1,25 @@
 module ActionDispatch::Routing
   class Mapper
     def mount_graphql_devise_for(resource, opts = {})
-      custom_operations = opts[:operations] || {}
+      custom_operations  = opts[:operations] || {}
+      skipped_operations = opts.fetch(:skip, [])
+
+      default_mutations = {
+        login:               GraphqlDevise::Mutations::Login,
+        logout:              GraphqlDevise::Mutations::Logout,
+        sign_up:             GraphqlDevise::Mutations::SignUp,
+        update_password:     GraphqlDevise::Mutations::UpdatePassword,
+        send_reset_password: GraphqlDevise::Mutations::SendPasswordReset
+      }.freeze
+      default_queries = {
+        confirm_account:      GraphqlDevise::Resolvers::ConfirmAccount,
+        check_password_token: GraphqlDevise::Resolvers::CheckPasswordToken
+      }
+      supported_operations = default_mutations.keys + default_queries.keys
+
+      unless skipped_operations.all? { |skipped| supported_operations.include?(skipped) }
+        raise GraphqlDevise::Error, 'Trying to skip a non supported operation. Check for typos.'
+      end
 
       path         = opts.fetch(:at, '/graphql_auth')
       mapping_name = resource.underscore.tr('/', '_').to_sym
@@ -16,15 +34,7 @@ module ActionDispatch::Routing
                            "Types::#{resource}Type".safe_constantize ||
                            GraphqlDevise::Types::AuthenticableType
 
-      default_mutations = {
-        login:               GraphqlDevise::Mutations::Login,
-        logout:              GraphqlDevise::Mutations::Logout,
-        sign_up:             GraphqlDevise::Mutations::SignUp,
-        update_password:     GraphqlDevise::Mutations::UpdatePassword,
-        send_reset_password: GraphqlDevise::Mutations::SendPasswordReset
-      }.freeze
-
-      default_mutations.each do |action, mutation|
+      default_mutations.except(*skipped_operations).each do |action, mutation|
         used_mutation = if custom_operations[action].present?
           custom_operations[action]
         else
@@ -39,12 +49,7 @@ module ActionDispatch::Routing
         GraphqlDevise::Types::MutationType.field("#{mapping_name}_#{action}", mutation: used_mutation)
       end
 
-      default_queries = {
-        confirm_account:      GraphqlDevise::Resolvers::ConfirmAccount,
-        check_password_token: GraphqlDevise::Resolvers::CheckPasswordToken
-      }
-
-      default_queries.each do |action, query|
+      default_queries.except(*skipped_operations).each do |action, query|
         used_query = if custom_operations[action].present?
           custom_operations[action]
         else
--- spec/requests/queries/check_password_token_spec.rb
+++ spec/requests/queries/check_password_token_spec.rb
@@ -5,76 +5,103 @@ RSpec.describe 'Check Password Token Requests' do
 
   let(:user)         { create(:user, :confirmed) }
   let(:redirect_url) { 'https://google.com' }
-  let(:query) do
-    <<-GRAPHQL
-      query {
-        userCheckPasswordToken(
-          resetPasswordToken: "#{token}",
-          redirectUrl: "#{redirect_url}"
-        ) {
-          email
+
+  context 'when using the user model' do
+    let(:query) do
+      <<-GRAPHQL
+        query {
+          userCheckPasswordToken(
+            resetPasswordToken: "#{token}",
+            redirectUrl: "#{redirect_url}"
+          ) {
+            email
+          }
         }
-      }
-    GRAPHQL
-  end
-
-  context 'when reset password token is valid' do
-    let(:token) { user.send(:set_reset_password_token) }
-
-    context 'when redirect_url is not provided' do
-      let(:redirect_url) { nil }
-
-      it 'returns authenticable and credentials in the headers' do
-        get_request
-
-        expect(response).to include_auth_headers
-        expect(json_response[:data][:userCheckPasswordToken]).to match(
-          email: user.email
-        )
-      end
+      GRAPHQL
     end
 
-    context 'when redirect url is provided' do
-      it 'redirects to redirect url' do
-        expect do
+    context 'when reset password token is valid' do
+      let(:token) { user.send(:set_reset_password_token) }
+
+      context 'when redirect_url is not provided' do
+        let(:redirect_url) { nil }
+
+        it 'returns authenticable and credentials in the headers' do
           get_request
 
-          user.reload
-        end.to change { user.tokens.keys.count }.from(0).to(1).and(
-          change(user, :allow_password_change).from(false).to(true)
-        )
+          expect(response).to include_auth_headers
+          expect(json_response[:data][:userCheckPasswordToken]).to match(
+            email: user.email
+          )
+        end
+      end
 
-        expect(response).to      redirect_to %r{\Ahttps://google.com}
-        expect(response.body).to include("client=#{user.reload.tokens.keys.first}")
-        expect(response.body).to include('access-token=')
-        expect(response.body).to include('uid=')
-        expect(response.body).to include('expiry=')
+      context 'when redirect url is provided' do
+        it 'redirects to redirect url' do
+          expect do
+            get_request
+
+            user.reload
+          end.to change { user.tokens.keys.count }.from(0).to(1).and(
+            change(user, :allow_password_change).from(false).to(true)
+          )
+
+          expect(response).to      redirect_to %r{\Ahttps://google.com}
+          expect(response.body).to include("client=#{user.reload.tokens.keys.first}")
+          expect(response.body).to include('access-token=')
+          expect(response.body).to include('uid=')
+          expect(response.body).to include('expiry=')
+        end
+      end
+
+      context 'when token has expired' do
+        it 'returns an expired token error' do
+          travel_to 10.hours.ago do
+            token
+          end
+
+          get_request
+
+          expect(json_response[:errors]).to contain_exactly(
+            hash_including(message: 'Reset password token is no longer valid.', extensions: { code: 'USER_ERROR' })
+          )
+        end
       end
     end
 
-    context 'when token has expired' do
-      it 'returns an expired token error' do
-        travel_to 10.hours.ago do
-          token
-        end
+    context 'when reset password token is not found' do
+      let(:token) { user.send(:set_reset_password_token) + 'invalid' }
 
+      it 'redirects to redirect url' do
         get_request
 
         expect(json_response[:errors]).to contain_exactly(
-          hash_including(message: 'Reset password token is no longer valid.', extensions: { code: 'USER_ERROR' })
+          hash_including(message: 'No user found for the specified reset token.', extensions: { code: 'USER_ERROR' })
         )
       end
     end
   end
 
-  context 'when reset password token is not found' do
-    let(:token) { user.send(:set_reset_password_token) + 'invalid' }
+  context 'when using the admin model' do
+    let(:token) { 'not_important' }
+    let(:query) do
+      <<-GRAPHQL
+        query {
+          adminCheckPasswordToken(
+            resetPasswordToken: "#{token}",
+            redirectUrl: "#{redirect_url}"
+          ) {
+            email
+          }
+        }
+      GRAPHQL
+    end
 
-    it 'redirects to redirect url' do
-      get_request
+    before { post_request }
 
+    it 'skips the sign up mutation' do
       expect(json_response[:errors]).to contain_exactly(
-        hash_including(message: 'No user found for the specified reset token.', extensions: { code: 'USER_ERROR' })
+        hash_including(message: "Field 'adminCheckPasswordToken' doesn't exist on type 'Query'")
       )
     end
   end
