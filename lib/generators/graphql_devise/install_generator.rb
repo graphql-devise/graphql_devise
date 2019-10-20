2a39d0d42a8240da6244c6de76a47906c2dc45b2
David Revelo <david@aperturaradical.org>
2019-10-20 14:34:23 -0500

    Replace generated DTA route with gqld route

--- lib/generators/graphql_devise/install_generator.rb
+++ lib/generators/graphql_devise/install_generator.rb
@@ -5,11 +5,20 @@ module GraphqlDevise
     argument :user_class, type: :string, default: 'User'
     argument :mount_path, type: :string, default: 'auth'
 
+    def create_devise_initializer
+      generate 'devise:install'
+    end
+
+    def execute_dta_installer
+      generate 'devise_token_auth:install', "#{user_class} #{mount_path}"
+    end
+
     def mount_resource_route
       routes_file = 'config/routes.rb'
       routes_path = File.join(destination_root, routes_file)
       gem_helper  = 'mount_graphql_devise_for'
       gem_route   = "#{gem_helper} '#{user_class}', at: '#{mount_path}'"
+      dta_route   = "mount_devise_token_auth_for '#{user_class}', at: '#{mount_path}'"
       file_start  = 'Rails.application.routes.draw do'
 
       if File.exist?(routes_path)
@@ -18,15 +27,17 @@ module GraphqlDevise
         if current_route.present?
           say_status('skipped', "Routes already exist for #{user_class} at #{mount_path}")
         else
-          insert_text_after_line(routes_path, file_start, gem_route)
+          current_dta_route = parse_file_for_line(routes_path, dta_route)
+
+          if current_dta_route.present?
+            replace_line(routes_path, dta_route, gem_route)
+          else
+            insert_text_after_line(routes_path, file_start, gem_route)
+          end
         end
       else
         say_status('skipped', "#{routes_file} not found. Add \"#{gem_route}\" to your routes file.")
       end
-    end
-
-    def create_devise_initializer
-      generate 'devise:install'
     end
 
     private
@@ -35,6 +46,10 @@ module GraphqlDevise
       gsub_file(filename, /(#{Regexp.escape(line)})/mi) do |match|
         "#{match}\n  #{str}"
       end
+    end
+
+    def replace_line(filename, old_line, new_line)
+      gsub_file(filename, /(#{Regexp.escape(old_line)})/mi, "  #{new_line}")
     end
 
     def parse_file_for_line(filename, str)
--- spec/generators/graphql_devise/install_generator_spec.rb
+++ spec/generators/graphql_devise/install_generator_spec.rb
@@ -11,25 +11,33 @@ RSpec.describe GraphqlDevise::InstallGenerator, type: :generator do
 
   let(:routes_path)    { "#{destination_root}/config/routes.rb" }
   let(:routes_content) { File.read(routes_path) }
+  let(:dta_route)      { "mount_devise_token_auth_for 'User', at: 'auth'" }
 
   context 'when the file exists' do
-    before { create_file_with_content(routes_path, "Rails.application.routes.draw do\n\nend") }
+    before do
+      create_file_with_content(
+        routes_path,
+        "Rails.application.routes.draw do\n#{dta_route}\nend"
+      )
+    end
 
     context 'when passing no params to the generator' do
       before { run_generator }
 
-      it 'add the routes using the default values for class and path' do
+      it 'replaces dta route using the default values for class and path' do
         generator_added_route = /  mount_graphql_devise_for 'User', at: 'auth'/
         expect(routes_content).to match(generator_added_route)
+        expect(routes_content).not_to match(dta_route)
       end
     end
 
     context 'when passing custom params to the generator' do
       before { run_generator %w[Admin api] }
 
-      it 'add the routes using the provided values for class and path' do
+      it 'add the routes using the provided values for class and path and keeps dta route' do
         generator_added_route = /  mount_graphql_devise_for 'Admin', at: 'api'/
         expect(routes_content).to match(generator_added_route)
+        expect(routes_content).to match(dta_route)
       end
     end
   end
