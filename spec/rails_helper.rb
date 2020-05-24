db84826c0a86633cbba9060da1ee330cc44e6ab0
Mario Celi <mcelicalderon@gmail.com>
2020-05-24 15:46:40 -0500

    Increase GraphqlDevise::GraphqlController coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,6 @@ RSpec.configure do |config|
   config.include(Requests::JsonHelpers, type: :request)
   config.include(Requests::AuthHelpers, type: :request)
   config.include(ActiveSupport::Testing::TimeHelpers)
-  config.include(Generators::FileHelpers, type: :generator)
 
   config.before(:suite) do
     ActionController::Base.allow_forgery_protection = true
