7aebf5fdaa48b648800bd409a22e38878b5f11cd
David Revelo <david@aperturaradical.org>
2020-06-17 08:36:24 -0500

    Fix broken reset password spec

--- spec/dummy/app/controllers/api/v1/graphql_controller.rb
+++ spec/dummy/app/controllers/api/v1/graphql_controller.rb
@@ -6,14 +6,41 @@ module Api
       before_action -> { set_resource_by_token(:user) }
 
       def graphql
-        render json: DummySchema.execute(params[:query], context: graphql_context)
+        result = DummySchema.execute(params[:query], execute_params(params))
+
+        render json: result unless performed?
       end
 
       def interpreter
-        render json: InterpreterSchema.execute(params[:query], context: graphql_context)
+        render json: InterpreterSchema.execute(params[:query], execute_params(params))
       end
 
       private
+
+      def execute_params(item)
+        {
+          operation_name: item[:operationName],
+          variables:      ensure_hash(item[:variables]),
+          context:        graphql_context
+        }
+      end
+
+      def ensure_hash(ambiguous_param)
+        case ambiguous_param
+        when String
+          if ambiguous_param.present?
+            ensure_hash(JSON.parse(ambiguous_param))
+          else
+            {}
+          end
+        when Hash, ActionController::Parameters
+          ambiguous_param
+        when nil
+          {}
+        else
+          raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
+        end
+      end
 
       def verify_authenticity_token
       end
