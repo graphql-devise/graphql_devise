d050585651f1dcefb3b173cdd9f7f4f7e7971277
David Revelo <david@aperturaradical.org>
2021-02-09 10:24:03 -0500

    Allow setting resource name in gql test shared context

--- spec/support/contexts/schema_test.rb
+++ spec/support/contexts/schema_test.rb
@@ -1,8 +1,11 @@
 RSpec.shared_context 'with graphql schema test' do
-  let(:variables)  { {} }
-  let(:resource)   { nil }
-  let(:controller) { instance_double(GraphqlDevise::GraphqlController) }
-  let(:context)    { { current_resource: resource, controller: controller } }
+  let(:variables)      { {} }
+  let(:resource_names) { [] }
+  let(:resource)       { nil }
+  let(:controller)     { instance_double(GraphqlDevise::GraphqlController) }
+  let(:context) do
+    { current_resource: resource, controller: controller, resource_name: resource_names }
+  end
   let(:response) do
     schema.execute(query, context: context, variables: variables).deep_symbolize_keys
   end
