1e434c11d1563df1fae7732eb4257352aa14adc4
David Revelo <david.revelo.uio@gmail.com>
2022-08-13 09:08:57 +0200

    Add spec for cookie setting

--- app/controllers/graphql_devise/application_controller.rb
+++ app/controllers/graphql_devise/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 module GraphqlDevise
-  class ApplicationController < Devise.parent_controller.constantize
+  ApplicationController = if Rails::VERSION::MAJOR >= 5
+    Class.new(ActionController::API)
+  else
+    Class.new(ActionController::Base)
   end
 end
