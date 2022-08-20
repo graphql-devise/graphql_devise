edd9839a19e736f4f12527a716fd4939f24863f0
David Revelo <david.revelo.uio@gmail.com>
2022-08-20 08:47:15 +0200

    Set base controller from route mount (investigation)

--- lib/graphql_devise/route_mounter.rb
+++ lib/graphql_devise/route_mounter.rb
@@ -1,6 +1,7 @@
 module GraphqlDevise
   module RouteMounter
     def mount_graphql_devise_for(resource, options = {})
+      GraphqlDevise.setup_base_controller(options.delete(:base_controller))
       clean_options = ResourceLoader.new(resource, options, true).call(
         Types::QueryType,
         Types::MutationType
