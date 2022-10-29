e444669fa2c606e5ae27dbacef180d0d78fc3490
David Revelo <david.revelo.uio@gmail.com>
2022-10-29 08:11:18 +0200

    Create controller on the fly when option is specified in mount

--- app/controllers/graphql_devise/application_controller.rb
+++ app/controllers/graphql_devise/application_controller.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 module GraphqlDevise
-  ApplicationController = Class.new(GraphqlDevise.base_controller)
-  # ApplicationController = if Rails::VERSION::MAJOR >= 5
-  #   Class.new(ActionController::API)
-  # else
-  #   Class.new(ActionController::Base)
-  # end
+  ApplicationController = if Rails::VERSION::MAJOR >= 5
+    Class.new(ActionController::API)
+  else
+    Class.new(ActionController::Base)
+  end
 end
--- lib/graphql_devise/route_mounter.rb
+++ lib/graphql_devise/route_mounter.rb
@@ -1,14 +1,23 @@
 module GraphqlDevise
   module RouteMounter
     def mount_graphql_devise_for(resource, options = {})
-      GraphqlDevise.setup_base_controller(options.delete(:base_controller))
+      routing = 'graphql_devise/graphql#auth'
+
+      if (base_controller = options.delete(:base_controller))
+        new_controller = GraphqlDevise.const_set("#{resource}AuthController", Class.new(base_controller))
+        new_controller.include(SetUserByToken)
+        new_controller.include(AuthControllerMethods)
+
+        routing = "#{new_controller.to_s.underscore.gsub('_controller','')}#auth"
+      end
+
       clean_options = ResourceLoader.new(resource, options, true).call(
         Types::QueryType,
         Types::MutationType
       )
 
-      post clean_options.at, to: 'graphql_devise/graphql#auth'
-      get  clean_options.at, to: 'graphql_devise/graphql#auth'
+      post clean_options.at, to: routing
+      get  clean_options.at, to: routing
     end
   end
 end
